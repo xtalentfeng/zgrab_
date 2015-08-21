95596c869380f77c746a52cc68bc2fdf9fc0b4d5
xtalentfeng <631958057@qq.com>
2015-08-21 22:33:03 +0800

    bug1.7

--- zlib/conn.go
+++ zlib/conn.go
@@ -640,9 +640,9 @@ func (c *Conn) SendS7Echo() (int,error){
 	event.size = res.size
 	event.Response = res.data
 	//event.parseSelf()
-	event.pdu_type = m.Response[1]
-	event.dst_tsap = int (binary.BigEndian.Uint16(m.Response[13:15]))
-	event.src_tsap = int (binary.BigEndian.Uint16(m.Response[9:11]))
+	event.pdu_type = event.Response[1]
+	event.dst_tsap = int (binary.BigEndian.Uint16(event.Response[13:15]))
+	event.src_tsap = int (binary.BigEndian.Uint16(event.Response[9:11]))
 
 	c.grabData.S7 = event
 	return w, err
